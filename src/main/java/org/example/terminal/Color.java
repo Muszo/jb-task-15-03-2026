b132f733a5aee813e2f375f3153621d83159c9e7
Muszo <zacharzmichal@gmail.com>
2026-03-15 09:47:00 +0100

    Add terminal cell data model (Color, Style, CellAttributes, Cell)

---
+++ src/main/java/org/example/terminal/Color.java
@@ -0,0 +1,24 @@
+package org.example.terminal;
+
+/**
+ * Represents the 16 standard terminal colors plus a default (unset) value.
+ */
+public enum Color {
+    DEFAULT,
+    BLACK,
+    RED,
+    GREEN,
+    YELLOW,
+    BLUE,
+    MAGENTA,
+    CYAN,
+    WHITE,
+    BRIGHT_BLACK,
+    BRIGHT_RED,
+    BRIGHT_GREEN,
+    BRIGHT_YELLOW,
+    BRIGHT_BLUE,
+    BRIGHT_MAGENTA,
+    BRIGHT_CYAN,
+    BRIGHT_WHITE
+}
